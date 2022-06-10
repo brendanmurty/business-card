07ed4c09aecdddd4ca4442d9a2dbd3fd682f7411
Brendan Murty <b@murty.io>
2022-06-10 11:36:47 +1000

    Add initial script contents

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 #!/usr/bin/env node
-// 👆 Used to tell Node.js that this is a CLI tool
 
 console.log(
-  "Create a CLI tool to output a business card in the terminal!\n\nView the tutorial ➞\n\nhttps://whitep4nth3r.com/blog/build-a-business-card-cli-tool/",
+  "\x1b[47m\x1b[36m\n %s \x1b[0m",
+  "Brendan Murty"
+);
+
+console.log(
+  "\x1b[47m\x1b[30m %s \x1b[0m",
+  "Father, Senior Software Engineer and Schnitzel Reviewer."
+);
+
+console.log(
+  "\x1b[47m\x1b[35m %s \x1b[0m",
+  "Website: https://brendan.murty.au"
+);
+
+console.log(
+  "\x1b[47m\x1b[35m %s \x1b[0m",
+  "GitHub: https://github.com/brendanmurty"
+);
+
+console.log(
+  "\x1b[47m\x1b[35m %s \x1b[0m",
+  "LinkedIn: https://linkedin.com/in/brendanmurty"
 );
