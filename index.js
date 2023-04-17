732bbcd200f9cff8e017e9242677ea3561debb72
Brendan Murty <brendan@boulevardglobal.com>
2023-04-17 16:00:58 +1000

    Remove grey text background colour

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 #!/usr/bin/env node
 
 console.log(
-  "\x1b[47m\x1b[36m\n %s \x1b[0m",
+  "\x1b[36m\n %s \x1b[0m",
   "Brendan Murty"
 );
 
 console.log(
-  "\x1b[47m\x1b[30m %s \x1b[0m",
+  "\x1b[30m %s \x1b[0m",
   "Father, Schnitzel Reviewer and Lead Software Engineer"
 );
 
 console.log(
-  "\x1b[47m\x1b[35m %s \x1b[0m",
+  "\x1b[35m %s \x1b[0m",
   "Email: brendan@murty.au"
 );
 
 console.log(
-  "\x1b[47m\x1b[35m %s \x1b[0m",
+  "\x1b[35m %s \x1b[0m",
   "Website: https://murty.au"
 );
 
 console.log(
-  "\x1b[47m\x1b[35m %s \x1b[0m",
+  "\x1b[35m %s \x1b[0m",
   "GitHub: https://github.com/brendanmurty"
 );
 
 console.log(
-  "\x1b[47m\x1b[35m %s \x1b[0m",
+  "\x1b[35m %s \x1b[0m",
   "LinkedIn: https://linkedin.com/in/brendanmurty"
 );
+
+console.log("");
